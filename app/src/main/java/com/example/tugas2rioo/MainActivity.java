e3fe13793d24db340eb5d67d03e33ed9af732ab9
intanptsr <41522010262@student.mercubuana.ac.id>
2024-04-23 19:18:17 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas2rioo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tugas2rioo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void loginreg(View view) {
+        Intent intent = new Intent(MainActivity.this, loginreg.class);
+        startActivity(intent);
+    }
+
+
+}
